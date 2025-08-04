9794d977f5abff7da02314d8148abf1cee8d390c
Julia Yume Nagahma Kriedte <j.nagahama@edu.pucrs.br>
2025-08-04 21:36:20 +0000

    Added Codificador Medio

---
+++ CodificadorForte.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+
+public class CodificadorSimples implements Codificador {
+    public String getNome() {
+        return "Codificador Simples";
+    }
+
+    public LocalDate getDataCriacao() {
+        return LocalDate.of(2025, 03, 13);
+    }
+
+    public int getNivelSeguranca(){
+        return 10;
+    }
+
+    public String codifica(String str) {
+        StringBuilder encoded = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            encoded.append((char) (c + 10);
+        }
+
+        return encoded.toString();
+    }
+
+    public String decodifica(String str) {
+        StringBuilder encoded = new StringBuilder();
+        
+        for (char c : str.toCharArray()) {
+            encoded.append((char) (c - 10);
+        }
+        
+        return encoded.toString();
+    }
+}
